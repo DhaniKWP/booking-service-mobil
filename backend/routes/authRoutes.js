b824ec8ee43f2cd02c54a7c4902d7b8f5164d5f4
DhaniKWP <dhanikusuma256@gmail.com>
2025-06-14 20:49:44 +0700

    mendaftarkan route resend otp di authcontroller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,5 +7,6 @@ router.post('/verify-otp', authController.verifyOTP);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
+router.post('/resend-otp', authController.resendOTP);
 
 module.exports = router;
